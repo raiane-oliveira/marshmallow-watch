0b3cb663c3a6a1206d0950d30e0c59f703962789
Raiane Oliveira <raiane.oliveira404@gmail.com>
2024-11-29 14:44:45 -0300

    fix: add unique react query key to genres fetch

--- src/entities/genres/api/use-get-movie-genres.ts
+++ src/entities/genres/api/use-get-movie-genres.ts
@@ -3,15 +3,17 @@ import { Locale } from "@/shared/i18n";
 import { useQuery } from "@tanstack/react-query";
 
 export function useGetMovieGenres(locale: Locale) {
-  const query = useQuery({
-    queryKey: ['genres', locale],
-    queryFn: async () => {
-      const res = await api.get<{ genres: Genre[] }>(`/genres/movie?lang=${locale}`)
-      return res.data
-    },
-    refetchOnWindowFocus: false,
-    refetchOnMount: false
-  })
+	const query = useQuery({
+		queryKey: ["genres", "movies", locale],
+		queryFn: async () => {
+			const res = await api.get<{ genres: Genre[] }>(
+				`/genres/movie?lang=${locale}`,
+			);
+			return res.data;
+		},
+		refetchOnWindowFocus: false,
+		refetchOnMount: false,
+	});
 
-  return query
+	return query;
 }
--- src/entities/genres/api/use-get-show-genres.ts
+++ src/entities/genres/api/use-get-show-genres.ts
@@ -3,15 +3,17 @@ import { Locale } from "@/shared/i18n";
 import { useQuery } from "@tanstack/react-query";
 
 export function useGetShowGenres(locale: Locale) {
-  const query = useQuery({
-    queryKey: ['genres', locale],
-    queryFn: async () => {
-      const res = await api.get<{ genres: Genre[] }>(`/genres/show?lang=${locale}`)
-      return res.data
-    },
-    refetchOnWindowFocus: false,
-    refetchOnMount: false
-  })
+	const query = useQuery({
+		queryKey: ["genres", "shows", locale],
+		queryFn: async () => {
+			const res = await api.get<{ genres: Genre[] }>(
+				`/genres/show?lang=${locale}`,
+			);
+			return res.data;
+		},
+		refetchOnWindowFocus: false,
+		refetchOnMount: false,
+	});
 
-  return query
+	return query;
 }
