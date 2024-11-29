8e816e30024a22713e36a9f261fddf4f1c0ad9e9
Raiane Oliveira <raiane.oliveira404@gmail.com>
2024-11-29 10:45:00 -0300

    fix: hit the react query cache instead of make an API call

--- src/entities/movies/api/use-discover-movies.ts
+++ src/entities/movies/api/use-discover-movies.ts
@@ -1,40 +1,43 @@
-import {
-	api,
-	Movie,
-} from "@/shared/api";
+import { api, Movie } from "@/shared/api";
 import { Locale } from "@/shared/i18n";
 import { useInfiniteQuery } from "@tanstack/react-query";
 
 interface DiscoverMoviesProps {
-  locale: Locale
-  genreIds: string[]
+	locale: Locale;
+	genreIds: string[];
 }
 
-export function useDiscoverMovies({locale = "en", genreIds}: DiscoverMoviesProps) {
+export function useDiscoverMovies({
+	locale = "en",
+	genreIds,
+}: DiscoverMoviesProps) {
 	const query = useInfiniteQuery({
 		queryKey: ["movies", ...genreIds],
 		queryFn: async ({ pageParam }) => {
-			const response = await api.get<{ movies: Movie[] }>(`/discover/movies?lang=${locale}&genreIds=${genreIds.join(",")}&page=${pageParam}`);
+			const response = await api.get<{ movies: Movie[] }>(
+				`/discover/movies?lang=${locale}&genreIds=${genreIds.join(",")}&page=${pageParam}`,
+			);
 
 			return response.data.movies;
 		},
-    initialPageParam: 1,
-    getNextPageParam: (lastPage, _allPages, lastPageParam) => {
-      if (lastPage.length === 0) {
-        return undefined
-      }
-      return lastPageParam + 1
-    },
-    getPreviousPageParam: (_firstPage, _allPages, firstPageParam) => {
-      if (firstPageParam <= 1) {
-        return undefined
-      }
-      return firstPageParam - 1
-    },
+		initialPageParam: 1,
+		getNextPageParam: (lastPage, _allPages, lastPageParam) => {
+			if (lastPage.length === 0) {
+				return undefined;
+			}
+			return lastPageParam + 1;
+		},
+		getPreviousPageParam: (_firstPage, _allPages, firstPageParam) => {
+			if (firstPageParam <= 1) {
+				return undefined;
+			}
+			return firstPageParam - 1;
+		},
+		staleTime: Infinity,
+		gcTime: 1000 * 60 * 10, // 10 min
 		refetchOnWindowFocus: false,
 		refetchOnMount: false,
 		refetchOnReconnect: false,
 	});
 	return query;
 }
-
